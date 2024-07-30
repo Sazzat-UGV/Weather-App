6eedd11fd716f8864759085d9a0643eaba043cff
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2024-07-31 01:09:53 +0600

    UI Design

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,117 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
+  <div class="container">
+    <div class="wrap">
+      <!-- search box for location-->
+      <div class="search-box">
+        <input type="text" placeholder="Search..." class="search-bar" />
+      </div>
+      <!-- weather information-->
+      <div class="weather-info">
+        <div class="location-box">
+          <div class="location">Dhaka</div>
+          <div class="date">17-12-2022</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">22 °C</div>
+          <div class="weather">askdjf</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+}
+
+.container {
+  background-image: url("./assets/background.jpg");
+  background-size: cover;
+  background-position: center;
+  transition: 0.4s;
+  width: 375px;
+  margin: 0 auto;
+  border-radius: 25px;
+  margin-top: 30px;
+  box-shadow: 0px 0px 30px #00000065;
+}
+
+.wrap {
+  height: 550px;
+  padding: 25px;
+  border-radius: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.4));
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  background-color: rgba(255, 255, 255, 0.5);
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  border-radius: 18px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+}
+
+.location-box .location {
+  color: white;
+  font-size: 32px;
+  font-weight: 500;
+  font-style: italic;
+  text-align: center;
+  margin-top: 30px;
+}
+
+.location-box .date {
+  color: white;
+  font-size: 20px;
+  font-weight: 300;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: white;
+  font-size: 100px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  font-style: italic;
+}
+
+.weather-box .weather {
+  color: white;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
 </style>
